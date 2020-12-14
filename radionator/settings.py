d1ff7c5a504d66ab4acdff3ae17faccd487807a3
pierstoyanov <0885095540gH!$!>
2020-12-14 19:26:01 +0200

    collectstatic

--- radionator/settings.py
+++ radionator/settings.py
@@ -129,13 +129,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     join(BASE_DIR, 'static'),
+# )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = join(BASE_DIR, 'media')
-
+STATIC_ROOT = join(BASE_DIR, 'static')
 LOGIN_URL = reverse_lazy('login')
 LOGIN_REDIRECT_URL = reverse_lazy('radio index')
 #
