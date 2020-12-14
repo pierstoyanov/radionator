06bef20aaa9b121ef2d4dab31ed105e47de3dc4c
pierstoyanov <0885095540gH!$!>
2020-12-14 20:55:52 +0200

    gunicorn & settings

--- radionator/settings.py
+++ radionator/settings.py
@@ -28,7 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'https://radionator.herokuapp.com/',
+]
 
 # Application definition
 
