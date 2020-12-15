7996a0d710bffef25bd81b3433f5431b3992d9e3
pierstoyanov <0885095540gH!$!>
2020-12-15 18:18:07 +0200

    debug false

--- radionator/settings.py
+++ radionator/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', None)
+DEBUG = os.environ.get('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split()
 
