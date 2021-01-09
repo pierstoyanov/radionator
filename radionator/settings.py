d638011b5e400c81432948204c3eeeb7bf193f82
pierstoyanov <0885095540gH!$!>
2021-01-10 01:09:30 +0200

    settings

--- radionator/settings.py
+++ radionator/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False) == 'True'
 
-# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split()
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split()
 
 # Application definition
 
